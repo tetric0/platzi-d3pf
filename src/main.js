b668108241b772e15d35ba30c239d48cab4f83c0
victor.munoz <victor.munoz@upc.edu>
2021-11-26 13:52:26 +0100

    Ejercicio '12. NavBar y Footer' del Curso Avanzado de Vue.js de Platzi completado.

--- src/main.js
+++ src/main.js
@@ -2,19 +2,42 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 
+// FontAwesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSkull, faCrown, faDungeon, faHatWizard, faHammer, faGem } from '@fortawesome/free-solid-svg-icons'
+import { faVuejs, faBootstrap, faFontAwesome, faGithub, faBattleNet } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 // Archivos locales de nuestra App
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-// CSS Global
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// CSS Global
 import './assets/css/main.styl'
+
+// Le añadimos los iconos que acabamos de importar (todos, los de tipo solid y de tipo brand)
+library.add(
+  faSkull,
+  faCrown,
+  faDungeon,
+  faHatWizard,
+  faHammer,
+  faGem,
+  faVuejs,
+  faBootstrap,
+  faFontAwesome,
+  faGithub,
+  faBattleNet
+)
 
 // Configuración extra
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 // Instancia principal de Vue
 new Vue({
