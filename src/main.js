2eaeb6a27603a28280aedf9364baf1c1c1da5320
victor.munoz <victor.munoz@upc.edu>
2021-11-29 09:48:20 +0100

    Ejercicio '13. Refactorizando' del Curso Avanzado de Vue.js de Platzi completado.

--- src/main.js
+++ src/main.js
@@ -1,52 +1,28 @@
-// Paquetes de npm
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
 
-// FontAwesome
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSkull, faCrown, faDungeon, faHatWizard, faHammer, faGem } from '@fortawesome/free-solid-svg-icons'
-import { faVuejs, faBootstrap, faFontAwesome, faGithub, faBattleNet } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// BootstrapVue
+import './plugins/bootstrapVue'
 
-// Archivos locales de nuestra App
+// Vue Font-Awesome
+import './plugins/fontAwesome'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 // CSS Global
 import './assets/css/main.styl'
 
-// Le añadimos los iconos que acabamos de importar (todos, los de tipo solid y de tipo brand)
-library.add(
-  faSkull,
-  faCrown,
-  faDungeon,
-  faHatWizard,
-  faHammer,
-  faGem,
-  faVuejs,
-  faBootstrap,
-  faFontAwesome,
-  faGithub,
-  faBattleNet
-)
-
-// Configuración extra
-Vue.use(BootstrapVue)
 Vue.config.productionTip = false
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 
-// Instancia principal de Vue
 new Vue({
   router,
+
   store,
 
   methods: {
     init () {
-      store.dispatch('oauth/getToken', null, { root: true })
+      store.dispatch('oauth/getToken')
     }
   },
 
